1859846441ea17d801b3e3e3351bd1e881c3f24d
Evgeny Pletnev <e.a.pletnev@gmail.com>
2020-08-08 18:53:33 +0100

    Added Particles component

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import './App.css';
 
 const particlesOptions =  {
   particles: {
-   line_linked: {
-     shadow: {
-       enable: true,
-       color: "#3CA9D1",
-       blur: 5
-     }
+    number: {
+     value: 50,
+     density: {
+      enable: true,
+      value_area: 800    
+    }
    }
   }
  }
 function App() {
   return (
     <div className="App">
-      <Particles 
+      <Particles className='particles'
        params={particlesOptions}             
       />
      <Navigation />
