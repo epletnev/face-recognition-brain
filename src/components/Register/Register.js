173c38b97147905f7e0eafb6032e2b223c53201c
Evgeny Pletnev <e.a.pletnev@gmail.com>
2020-09-16 19:22:33 +0100

    Update Register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
-const Register = ({ onRouteChange}) => {
-	return (
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    }
+  }
+
+  onNameChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  onEmailChange = (event) => {
+    this.setState({email: event.target.value})
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user) {
+          this.props.onRouteChange('home');
+        }
+      })    
+  }
+
+  render() {    
+	  return (
 	      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
@@ -14,6 +54,7 @@ const Register = ({ onRouteChange}) => {
                   type="text"
                   name="name"
                   id="name"
+                  onChange={this.onNameChange}
                 />
               </div>
               <div className="mt3">
@@ -23,7 +64,7 @@ const Register = ({ onRouteChange}) => {
                   type="email"
                   name="email-address"
                   id="email-address"
-                  //onChange={this.onEmailChange}
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
@@ -33,14 +74,13 @@ const Register = ({ onRouteChange}) => {
                   type="password"
                   name="password"
                   id="password"
-                  //onChange={this.onPasswordChange}
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
               <input
-                //onClick={this.onSubmitSignIn}
-                onClick={() => onRouteChange('home')}
+                onClick={this.onSubmitSignIn}                
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
@@ -50,6 +90,7 @@ const Register = ({ onRouteChange}) => {
         </main>
       </article>
 	);
+  }
 }
 
 export default Register;
