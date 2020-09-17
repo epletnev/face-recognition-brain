338f779cd99dd1933169d10318944398a6ef82bb
Evgeny Pletnev <e.a.pletnev@gmail.com>
2020-09-17 18:20:40 +0100

    Update App.js and Rank component

--- src/App.js
+++ src/App.js
@@ -34,10 +34,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',        
+        entries: 0,
+        joined: ''
+      }
     }
   }
-  
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,        
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
 calculateFaceLocation = (data) => {
   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
   const image = document.getElementById('inputimage');
@@ -61,8 +78,26 @@ calculateFaceLocation = (data) => {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+    .then(response => {
+      if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+        }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
   }
 
@@ -86,7 +121,7 @@ calculateFaceLocation = (data) => {
      { route === 'home'
      ? <div>
            <Logo />
-           <Rank />
+           <Rank name={this.state.user.name} entries={this.state.user.entries}/>
            <ImageLinkForm 
               onInputChange={this.onInputChange} 
               onButtonSubmit={this.onButtonSubmit}
@@ -95,8 +130,8 @@ calculateFaceLocation = (data) => {
         </div>
       : (
         route === 'signin'
-       ? <Signin onRouteChange={this.onRouteChange}/>
-       : <Register onRouteChange={this.onRouteChange}/>
+       ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+       : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
       )
    }
     </div>
