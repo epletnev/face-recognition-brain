182b533e4a58a29c059fd2bda56cd76f8c1efa20
Evgeny Pletnev <e.a.pletnev@gmail.com>
2020-09-24 17:41:36 +0100

    Update App.js and Rank component

--- src/App.js
+++ src/App.js
@@ -26,23 +26,25 @@ const particlesOptions =  {
   }
  }
 
+const initialState = {
+   input: '',
+   imageUrl: '',
+   box: {},
+   route: 'signin',
+   isSignedIn: false,
+   user: {
+    id: '',
+    name: '',
+    email: '',        
+    entries: 0,
+    joined: ''    
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',        
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -95,6 +97,7 @@ calculateFaceLocation = (data) => {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(console.log)
         }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
@@ -103,7 +106,7 @@ calculateFaceLocation = (data) => {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
